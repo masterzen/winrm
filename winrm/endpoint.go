14e40fab69cb97d7c70a2a2361e524d33483c038
hoenirvili <hoenirvili@gmail.com>
2016-04-21 16:53:49 +0300

    doc fix + idiomatic way for new endpoint struct

--- winrm/endpoint.go
+++ winrm/endpoint.go
@@ -2,6 +2,8 @@ package winrm
 
 import "fmt"
 
+// Endpoint struct holds configurations
+// for the server endpoint
 type Endpoint struct {
 	Host     string
 	Port     int
@@ -19,4 +21,15 @@ func (ep *Endpoint) url() string {
 	}
 
 	return fmt.Sprintf("%s://%s:%d/wsman", scheme, ep.Host, ep.Port)
+}
+
+// NewEndpoint returns new pointer to struct Endpoint
+func NewEndpoint(host string, port int, https bool, insecure bool, cert *[]byte) *Endpoint {
+	return &Endpoint{
+		Host:     host,
+		Port:     port,
+		HTTPS:    https,
+		Insecure: insecure,
+		CACert:   cert,
+	}
 }
