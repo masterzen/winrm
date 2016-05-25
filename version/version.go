6c49b512c3f98ea2c746bc507b755a825156b89c
Brice Figureau <brice@daysofwonder.com>
2016-05-25 18:22:31 +0200

    Add a version system
    Still missing a script to prepare and build releases

---
+++ version/version.go
@@ -0,0 +1,19 @@
+package version
+
+import (
+  "bytes"
+  "fmt"
+)
+
+var (
+  // Git SHA Value will be set during build
+  GitSHA = "N/A"
+  // update this when releasing a version
+  Version = "1.0.0"
+)
+
+func GetFullVersion() string {
+  var versionString bytes.Buffer
+  fmt.Fprintf(&versionString, "%s-%s", Version, GitSHA)
+  return versionString.String()
+}
